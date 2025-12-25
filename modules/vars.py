59e13118867198779001915b07fb0c03bb354c49
BeNegative <gtgm4j@gmail.com>
2025-12-25 19:14:19 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "26797881"))
-API_HASH = environ.get("API_HASH", "9699262c708c2e45ba18bfce925ed5ed")
+API_ID = int(environ.get("API_ID", "39256507"))
+API_HASH = environ.get("API_HASH", "d9f88a9d3d207b8f6dfb08b762c30965")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7973139452:AAF0WenzdTAGizg_7XX4ACINfWmBeXvv5ys")
 
-OWNER = int(environ.get("OWNER", "6567162029"))
-CREDIT = environ.get("CREDIT", "Nedits OP")
+OWNER = int(environ.get("OWNER", "7517872227"))
+CREDIT = environ.get("CREDIT", "YUS")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '8277882193').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7517872227').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8277882193').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7517872227').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
